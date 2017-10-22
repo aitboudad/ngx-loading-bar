7924a677069e243eafd56707dea9a18555624f8c
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-10-22 15:13:33 +0100

    remove deprecated methods

--- packages/core/src/loading-bar.service.ts
+++ packages/core/src/loading-bar.service.ts
@@ -1,8 +1,5 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
-import { Observer } from 'rxjs/Observer';
-import { Observable } from 'rxjs/Observable';
-import { share } from 'rxjs/operator/share';
 
 export interface LoadingBarStatus {
     started?: boolean;
@@ -31,34 +28,6 @@ export class LoadingBarService implements OnDestroy {
             completed: this._pendingRequests === 1,
             pendingRequests: --this._pendingRequests,
         });
-    }
-
-    startLoading(source$?: Observable<any>) {
-        console.warn(`LoadingBarService: 'startLoading' is deprecated and will removed in the next version use 'start' instead.`);
-        this.start();
-        if (source$) {
-            console.warn(`LoadingBarService: passing 'source$' to 'startLoading' is deprecated and will removed in the next version.`);
-
-            source$ = share.call(source$);
-            source$.subscribe(this.getLoadingObserver());
-
-            return source$;
-        }
-    }
-
-    endLoading() {
-        console.warn(`LoadingBarService: 'endLoading' is deprecated and will removed in the next version use 'complete' instead.`);
-        this.complete();
-    }
-
-    getLoadingObserver(): Observer<any> {
-        console.warn(`LoadingBarService: 'getLoadingObserver' is deprecated and will removed in the next version.`);
-
-        return {
-            next: (x) => x,
-            error: (err) => this.complete(),
-            complete: () => this.complete(),
-        };
     }
 
     ngOnDestroy() {
