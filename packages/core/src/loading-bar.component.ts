db382c70d9235cf6c1e2d520a01b99a53f4a85db
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-11-25 13:42:53 +0000

    feat(core): emit progress value instead of pending requests

--- packages/core/src/loading-bar.component.ts
+++ packages/core/src/loading-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { LoadingBarService } from './loading-bar.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'ng-loading-bar, ngx-loading-bar',
   template: `
-    <ng-container *ngIf="started">
+    <ng-container *ngIf="(progress$|async) as value">
       <div id="loading-bar-spinner" *ngIf="includeSpinner"><div class="spinner-icon"></div></div>
       <div id="loading-bar"><div class="bar" [style.width]="value + '%'"><div class="peg"></div></div></div>
     </ng-container>
@@ -12,109 +13,7 @@ import { LoadingBarService } from './loading-bar.service';
 })
 export class LoadingBarComponent {
   @Input() includeSpinner = true;
+  progress$: Subject<number> = this.loader.progress$;
 
-  value = 0;
-  started = false;
-
-  private _latencyThreshold = 10;
-  private _startSize = 0.02;
-  private _status = 0;
-
-  private _incTimeout: any;
-  private _completeTimeout: any;
-  private _startTimeout: any;
-
-  constructor(loadingBarService: LoadingBarService) {
-    loadingBarService.pending.subscribe((pendingRequests) => {
-      if (pendingRequests !== 0 && !this.started) {
-        this.start();
-      }
-
-      if (pendingRequests === 0) {
-        this.complete();
-      }
-    });
-  }
-
-  /**
-   * Inserts the loading bar element into the dom, and sets it to 2%
-   */
-  private start() {
-    this._startTimeout = setTimeout(() => {
-      clearTimeout(this._completeTimeout);
-
-      // do not continually broadcast the started event:
-      if (this.started) {
-          return;
-      }
-
-      this.started = true;
-      this._status = 0;
-
-      this.set(this._startSize);
-    }, this._latencyThreshold);
-  }
-
-  private complete() {
-    setTimeout(() => {
-      this.set(1);
-
-      clearTimeout(this._completeTimeout);
-      clearTimeout(this._startTimeout);
-
-      // Attempt to aggregate any start/complete calls within 500ms:
-      this._completeTimeout = setTimeout(() => this.started = false, 500);
-    }, this._latencyThreshold);
-  }
-
-  /**
-   * Set the loading bar's width to a certain percent.
-   *
-   * @param n any value between 0 and 1
-   */
-  private set(n) {
-    if (!this.started) {
-      return;
-    }
-
-    this.value = n * 100;
-    this._status = n;
-
-    // increment loadingbar to give the illusion that there is always
-    // progress but make sure to cancel the previous timeouts so we don't
-    // have multiple incs running at the same time.
-    clearTimeout(this._incTimeout);
-    this._incTimeout = setTimeout(() => this.increment(), 250);
-  }
-
-  /**
-   * Increments the loading bar by a random amount
-   * but slows down as it progresses
-   */
-  private increment() {
-    if (this._status >= 1) {
-      return;
-    }
-
-    let rnd = 0;
-    const stat = this._status;
-    if (stat >= 0 && stat < 0.25) {
-      // Start out between 3 - 6% increments
-      rnd = (Math.random() * (5 - 3 + 1) + 3) / 100;
-    } else if (stat >= 0.25 && stat < 0.65) {
-      // increment between 0 - 3%
-      rnd = (Math.random() * 3) / 100;
-    } else if (stat >= 0.65 && stat < 0.9) {
-      // increment between 0 - 2%
-      rnd = (Math.random() * 2) / 100;
-    } else if (stat >= 0.9 && stat < 0.99) {
-      // finally, increment it .5 %
-      rnd = 0.005;
-    } else {
-      // after 99%, don't increment:
-      rnd = 0;
-    }
-
-    this.set(this._status + rnd);
-  }
+  constructor(private loader: LoadingBarService) {}
 }
--- packages/core/src/loading-bar.service.ts
+++ packages/core/src/loading-bar.service.ts
@@ -3,11 +3,25 @@ import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class LoadingBarService implements OnDestroy {
-  pending = new Subject<number>();
+  readonly progress$ = new Subject<number>();
+
+  private _pending = new Subject<number>();
   private _pendingRequests = 0;
+  private _value = 0;
+  private _incTimeout: any;
+
+  get pending() {
+    console.warn(`LoadingBarService: 'pending' is deprecated and will removed in the next version use 'progress$' instead which return a value between 0 and 100.`);
+
+    return this._pending;
+  }
 
   start() {
-    this.pending.next(++this._pendingRequests);
+    this._pending.next(++this._pendingRequests);
+    if (this._value === 0 || this._value === 100) {
+      // Inserts the loading bar element into the dom, and sets it to 2%
+      this.set(2);
+    }
   }
 
   complete() {
@@ -15,10 +29,69 @@ export class LoadingBarService implements OnDestroy {
       return;
     }
 
-    this.pending.next(--this._pendingRequests);
+    this._pending.next(--this._pendingRequests);
+
+    if (this._pendingRequests === 0 && this._value !== 100) {
+      if (this._value > 0) {
+        this.set(100);
+        // Attempt to aggregate any start/complete calls within 500ms:
+        setTimeout(() => {
+          if (this._value === 100) {
+            this.progress$.next(0);
+            this._value = 0;
+          }
+        }, 500);
+      }
+    }
   }
 
   ngOnDestroy() {
-      this.pending.complete();
+    this._pending.complete();
+    this.progress$.complete();
+  }
+
+  /**
+ * Set the loading bar's width to a certain percent.
+ *
+ * @param n any value between 0 and 100
+ */
+  private set(n) {
+    this._value = n;
+    this.progress$.next(n);
+
+    // increment loadingbar to give the illusion that there is always
+    // progress but make sure to cancel the previous timeouts so we don't
+    // have multiple incs running at the same time.
+    clearTimeout(this._incTimeout);
+    if (this._value < 100) {
+      this._incTimeout = setTimeout(() => this.increment(), 250);
+    }
+  }
+
+  /**
+   * Increments the loading bar by a random amount
+   * but slows down as it progresses
+   */
+  private increment() {
+    let rnd = 0;
+    const stat = this._value;
+    if (stat >= 0 && stat < 25) {
+      // Start out between 3 - 6% increments
+      rnd = (Math.random() * (5 - 3 + 1) + 3);
+    } else if (stat >= 25 && stat < 65) {
+      // increment between 0 - 3%
+      rnd = (Math.random() * 3);
+    } else if (stat >= 65 && stat < 90) {
+      // increment between 0 - 2%
+      rnd = (Math.random() * 2);
+    } else if (stat >= 90 && stat < 99) {
+      // finally, increment it .5 %
+      rnd = 0.5;
+    } else {
+      // after 99%, don't increment:
+      rnd = 0;
+    }
+
+    this.set(this._value + rnd);
   }
 }
