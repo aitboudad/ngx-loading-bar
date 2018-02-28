30567cce774e3087944e73a9b5164aeb95b21c13
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-02-28 09:14:21 +0000

    feat(core): allow setting height and diameter for loading bar/spinner.
    fix #62

--- packages/core/src/loading-bar.component.ts
+++ packages/core/src/loading-bar.component.ts
@@ -5,10 +5,12 @@ import { LoadingBarService } from './loading-bar.service';
   selector: 'ngx-loading-bar',
   template: `
     <ng-container *ngIf="(loader.progress$|async) as progress">
-      <div id="loading-bar-spinner" *ngIf="includeSpinner" [style.color]="color"><div class="spinner-icon"></div></div>
+      <div id="loading-bar-spinner" *ngIf="includeSpinner" [style.color]="color">
+        <div [style.width]="diameter" [style.height]="diameter" class="spinner-icon"></div>
+      </div>
       <div id="loading-bar" *ngIf="includeBar" [style.color]="color">
-        <div class="bar" [style.background]="color" [style.width]="progress + '%'">
-          <div class="peg"></div>
+        <div class="bar" [style.background]="color" [style.height]="height" [style.width]="progress + '%'">
+          <div class="peg" [style.height]="height"></div>
         </div>
       </div>
     </ng-container>
@@ -20,6 +22,8 @@ export class LoadingBarComponent {
   @Input() includeSpinner = true;
   @Input() includeBar = true;
   @Input() color;
+  @Input() height;
+  @Input() diameter;
 
   constructor(public loader: LoadingBarService) {}
 }
