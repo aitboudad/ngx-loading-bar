09a9bf695d6588bd174870becf3d117e2c32207f
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2018-11-22 12:11:24 +0100

    feat: set peerDependency to angular >=7.0
    BREAKING CHANGE: The library now requires Angular 7

--- .config/build.js
+++ .config/build.js
@@ -1,15 +1,16 @@
 const fs = require("fs"),
-    packages = [
-        'core',
-        'http',
-        'http-client',
-        'router',
-    ];
+  packages = [
+    'core',
+    'http',
+    'http-client',
+    'router',
+  ];
 
 // update `LOADING-BAR-VERSION` in package.json for all sub-packages
 const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
 packages.map(package => {
-    const packagePath = `dist/${package}/package.json`;
-    package = fs.readFileSync(packagePath, 'utf8');
-    fs.writeFileSync(packagePath, package.replace(/LOADING-BAR-VERSION/g, version));
+  const packagePath = `dist/@ngx-loading-bar/${package}/package.json`;
+
+  package = fs.readFileSync(packagePath, 'utf8');
+  fs.writeFileSync(packagePath, package.replace(/FORMLY-VERSION|0.0.0/g, version));
 });
