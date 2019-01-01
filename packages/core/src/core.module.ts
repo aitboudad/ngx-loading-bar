fba3a919f5df7c4ed07d5cd932a738cfd7e581a9
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2019-01-01 11:40:31 +0100

    feat: remove deprecated forRoot
    BREAKING CHANGE: the `forRoot` method on `LoadingBarModule` has been removed.

--- packages/core/src/core.module.ts
+++ packages/core/src/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingBarComponent } from './loading-bar.component';
 
@@ -7,11 +7,4 @@ import { LoadingBarComponent } from './loading-bar.component';
   declarations: [LoadingBarComponent],
   exports: [LoadingBarComponent],
 })
-export class LoadingBarModule {
-  /** @deprecated */
-  static forRoot(): ModuleWithProviders {
-    console.warn('The `LoadingBarModule.forRoot()` calls is deprecated, use `LoadingBarModule` instead');
-
-    return { ngModule: LoadingBarModule };
-  }
-}
+export class LoadingBarModule {}
