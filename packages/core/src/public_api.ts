ba350cbe830b6ef42237285807ae862b90179073
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2020-07-11 19:46:26 +0100

    feat(core): add option to customise latency threshold
    fix #158, fix #166

--- packages/core/src/public_api.ts
+++ packages/core/src/public_api.ts
@@ -1,3 +1,4 @@
 export { LoadingBarModule } from './core.module';
 export { LoadingBarComponent } from './loading-bar.component';
 export { LoadingBarService } from './loading-bar.service';
+export { LOADING_BAR_CONFIG, LoadingBarConfig } from './loading-bar.config';
